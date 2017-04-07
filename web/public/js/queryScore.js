6f5554c9b8daa8e736af14e0d72ab12280bed7dc
gaoyx <melo@gaoyuexiang.cn>
2017-04-07 15:41:58 +0800

    add load to add classes

--- web/public/js/queryScore.js
+++ web/public/js/queryScore.js
@@ -1,10 +1,55 @@
 'use strict';
 
-import $ from 'jquery.min';
+// import $ from 'jquery.min';
 
 const allClassesUrl = '/classes';
 
-$.load(allClassesUrl, (classes) => {
-  classes.map(clazz => {
-  })
+const formatStudents = students => {
+  return students.map(student => {
+    return `
+<tr>
+<td>${student.name}</td>
+<td>${student.subject.math}</td>
+<td>${student.subject.chinese}</td>
+<td>${student.subject.english}</td>
+<td>${student.subject.program}</td>
+<td>${student.subject.calAverage()}</td>
+<td>${student.subject.calTotal()}</td>
+</tr>
+    `;
+  }).join('');
+};
+
+$('#classes').load(allClassesUrl, (classes) => {
+  const outputHTML = classes.map(clazz => {
+    return `
+<div>班级: <span>${clazz.classNumber}</span></div>
+<div>
+  <table class="table">
+    <tr>
+      <th>姓名</th>
+      <th>数学成绩</th>
+      <th>语文成绩</th>
+      <th>英语成绩</th>
+      <th>编程成绩</th>
+      <th>平均分</th>
+      <th>总分</th>
+    </tr>
+    ${formatStudents(clazz.students)}
+  </table>
+</div>
+<div>
+  <table class="table">
+    <tr>
+      <th>班级总分平均分</th>
+      <th>班级总分中位数</th>
+    </tr>
+    <tr>
+      <td>${clazz.average}</td>
+      <td>${clazz.median}</td>
+    </tr>
+  </table>
+</div>`;
+  }).join('');
+  $('#classes').html(outputHTML);
 });
